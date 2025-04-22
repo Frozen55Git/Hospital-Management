0f7fc481269108b9ff799322e207ff0ae8392b19
Frozen55Git <michael.wisis0505@gmail.com>
2025-04-22 22:25:21 +0900

    Update Departments

--- Departments/admin.py
+++ Departments/admin.py
@@ -8,7 +8,6 @@ from django.utils.translation import gettext as _, gettext_lazy
 class DoctorsAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'position', 'joined', 'email',
                     'phone_number', 'specialty']
-    list_filter = ['first_name', 'last_name', 'phone_number']
     search_fields = ['first_name', 'last_name', 'phone_number']
     list_per_page = 50
 
